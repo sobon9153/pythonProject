2c2674fec219bf176c15474e4f8f451bc9cf5439
Mateusz Sobon <mateusz.sobon@studenic.collegiumwitelona.pl>
2023-11-17 11:01:51 +0100

    add second window wich we can to introduce Azymut T

--- szkoła/ZGZ or ZGO/ZGZ_ZGO_Zgit.py
+++ szkoła/ZGZ or ZGO/ZGZ_ZGO_Zgit.py
@@ -1,6 +1,39 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+
+
+class OknoZGZ(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.setGeometry(200, 200, 400, 200)
+        self.setWindowTitle('ZGZ - Podaj Azymut')
+
+        self.azymut_label = QLabel('Podaj azymut T z punktu do punktu B:', self)
+        self.azymut_input = QLineEdit(self)
+        self.azymut_button = QPushButton('OK', self)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.azymut_label)
+        layout.addWidget(self.azymut_input)
+        layout.addWidget(self.azymut_button)
+
+        self.setLayout(layout)
+
+        self.azymut_button.clicked.connect(self.ok_button_clicked)
+
+    def ok_button_clicked(self):
+        try:
+            azymut = int(self.azymut_input.text())
+            print(f"Wprowadzony azymut: {azymut}")
+        except ValueError:
+            print("Podana nieprawidłowa wartość. Spróbuj jeszcze raz.")
+
+        self.close()
 
 
 class MojaAplikacja(QWidget):
@@ -10,7 +43,7 @@ class MojaAplikacja(QWidget):
 
     def init_ui(self):
         self.setGeometry(100, 100, 400, 300)
-        self.setWindowTitle('kalkulator dla Topo')
+        self.setWindowTitle('Kalkulator dla Topo')
 
         zgzs_button = QPushButton('ZGZ', self)
         zgzo_button = QPushButton('ZGO', self)
@@ -33,9 +66,12 @@ class MojaAplikacja(QWidget):
                                        ' na podstawie współrzędnych prostokątnych punktu wyjściowego (punktu A) oraz\n'
                                        ' azymutu (T-AB) i odległości (-dAB-) między nimi.')
 
+        self.okno_zgz = OknoZGZ()
+        self.okno_zgz.show()
+
     def zgzo_clicked(self):
-        self.objasnienie_label.setText('Istotą zadania geodezyjnego odwrotnego (ZGO)\n jest wyznaczanie azymutu '
-                                       'topograficznego (TAB) i odległości (-dAB-) między punktami A i B na podstawie\n'
+        self.objasnienie_label.setText('Istotą zadania geodezyjnego odwrotnego (ZGO)-\n jest wyznaczanie azymutu '
+                                       'topograficznego (TAB) i odległości (-dAB-) między punktami A i B na podstawie\n '
                                        'współrzędnych prostokątnych punktu A oraz współrzędnych prostokątnych punktu '
                                        'B.')
 
