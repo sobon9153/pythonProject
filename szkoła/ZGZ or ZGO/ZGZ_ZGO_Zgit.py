7d12d6f09d9046e70ba0d01f60a9cbac48c472d0
Mateusz Sobon <mateusz.sobon@studenic.collegiumwitelona.pl>
2023-11-19 16:09:49 +0100

    I added a second window with the ability to specify the azimuth and the return option. now I want it to show the error of incorrectly entered data - that is, window 3

--- szkoła/ZGZ or ZGO/ZGZ_ZGO_Zgit.py
+++ szkoła/ZGZ or ZGO/ZGZ_ZGO_Zgit.py
@@ -1,39 +1,47 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QLabel, QPushButton, QWidget, QLineEdit
 
 
 class OknoZGZ(QWidget):
-    def __init__(self):
+    def __init__(self, poprzednie_okno):
         super().__init__()
 
+        self.poprzednie_okno = poprzednie_okno
         self.init_ui()
 
     def init_ui(self):
         self.setGeometry(200, 200, 400, 200)
         self.setWindowTitle('ZGZ - Podaj Azymut')
 
-        self.azymut_label = QLabel('Podaj azymut T z punktu do punktu B:', self)
-        self.azymut_input = QLineEdit(self)
-        self.azymut_button = QPushButton('OK', self)
+        azymut_label = QLabel('Podaj azymut T z punktu do punktu B:', self)
+        azymut_input = QLineEdit(self)
+        azymut_button = QPushButton('OK', self)
+        powrot_button = QPushButton('Powrót', self)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.azymut_label)
-        layout.addWidget(self.azymut_input)
-        layout.addWidget(self.azymut_button)
+        layout.addWidget(azymut_label)
+        layout.addWidget(azymut_input)
+        layout.addWidget(azymut_button)
+        layout.addWidget(powrot_button)
 
         self.setLayout(layout)
 
-        self.azymut_button.clicked.connect(self.ok_button_clicked)
+        azymut_button.clicked.connect(self.ok_button_clicked)
+        powrot_button.clicked.connect(self.powrot_button_clicked)
 
     def ok_button_clicked(self):
         try:
-            azymut = int(self.azymut_input.text())
+            azymut = int(self.findChild(QLineEdit).text())
             print(f"Wprowadzony azymut: {azymut}")
         except ValueError:
             print("Podana nieprawidłowa wartość. Spróbuj jeszcze raz.")
 
         self.close()
+
+    def powrot_button_clicked(self):
+        self.close()
+        self.poprzednie_okno.show()
 
 
 class MojaAplikacja(QWidget):
@@ -66,12 +74,12 @@ class MojaAplikacja(QWidget):
                                        ' na podstawie współrzędnych prostokątnych punktu wyjściowego (punktu A) oraz\n'
                                        ' azymutu (T-AB) i odległości (-dAB-) między nimi.')
 
-        self.okno_zgz = OknoZGZ()
+        self.okno_zgz = OknoZGZ(self)
         self.okno_zgz.show()
 
     def zgzo_clicked(self):
         self.objasnienie_label.setText('Istotą zadania geodezyjnego odwrotnego (ZGO)-\n jest wyznaczanie azymutu '
-                                       'topograficznego (TAB) i odległości (-dAB-) między punktami A i B na podstawie\n '
+                                       'topograficznego (TAB) i odległości (-dAB-) między punktami A i B na podstawie\n'
                                        'współrzędnych prostokątnych punktu A oraz współrzędnych prostokątnych punktu '
                                        'B.')
 
